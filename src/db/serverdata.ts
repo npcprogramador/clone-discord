14ff219944ba2ec9324b607bc714db1f445090f6
npcwalison <gwalisonla@gmail.com>
2023-05-17 10:48:37 -0300

    update

---
+++ src/db/serverdata.ts
@@ -0,0 +1,21 @@
+export var ServerData = {
+    "minecraft": {
+        "ID": 1,
+        "name": "Minecraft",
+        "image": "https://cdn.icon-icons.com/icons2/2699/PNG/512/minecraft_logo_icon_168974.png",
+        //os membros contem mais dados, como cargo e acessos, mas isso será deixado para depois
+        "members": ["Lucas Viana", "Matheus Castro", "Vitória Martins", "Luana Matos", "Marcos Daniel", "Ivone Sampaio"],
+        "mesagens": {//organizar as mensagens por ordem de tempo
+            "Mateus Castro": {
+                "data": "12.03.2023",
+                "time": "22:45",
+                "mensage": "Olá meu amigo, como você está?"
+            },
+            "Lucas Viana": {
+                "data": "12.03.2023",
+                "time": "22:48",
+                "mensage": "Estou bem, e como vai você ?"
+            }
+        }
+    }
+}
